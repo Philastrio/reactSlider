67a135c8e03a99ebd96498c04171f641d2f2154e
sideproject0214 <sideproject0214@gmail.com>
2020-12-13 19:23:55 +0900

    react slider

---
+++ src/components/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1601758124360-86f5a0432c42?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1602526213372-376ca17a8e02?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1607473332998-e53c0c6f0ab0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80",
+  },
+];
